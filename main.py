8fa545bda5de7ca3802d690658fce0b76a08f4d6
Mariam <mari.kifshidze@gmail.com>
2023-11-14 18:36:56 +0400

    Learning Fast API

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Student(BaseModel):
+    id: int
+    name: str = Field(None, title="name of student", max_length=10)
+    subjects: List[str] = []
+
+
+@app.get("/")
+async def index():
+    return {"message": "Hello World"}
+
+
+@app.post("/students/")
+async def student_data(s1: Student):
+    return s1
